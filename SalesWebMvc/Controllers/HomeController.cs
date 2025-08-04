a83d2e7ec6c2c3d19fd645fcc738868d39b210f6
Esdras Marques <esdrasmqs.pro@gmail.com>
2025-08-04 16:27:13 -0300

    Adicionado informações sobre contato em About

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App C# Course";
+            ViewData["Aluno"] = "Esdras de Oliveira Marques";
 
             return View();
         }
